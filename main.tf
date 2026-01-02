3b25de2f88dba363248aad02e052437ee29e7b34
SrikanthErgula <srikanthreddybtech17@gmail.com>
2026-01-02 07:32:06 +0530

    terraform-VPC-module

---
+++ main.tf
@@ -0,0 +1,32 @@
+module "vpc_test" {
+  source = "../terraform-AWS-VPC-module"
+#    vpc_cidr = "10.0.0.0/16"
+#    project_name = "demo-roboshop"
+#    environment = "dev"
+
+# {above values commet endhuku ante vallaki vere value replce 
+#cheyalnate kastum so we need to give chance to them }
+
+   # VPC
+    vpc_cidr = var.vpc_test_cidr
+    project_name = var.project_test_name
+    environment = var.environment_test
+    vpc_tags = var.vpc_test_tags
+
+    # For public subnets
+    public_subnet_cidrs = var.public_subnet_cidrs # see in notes
+
+    # Private Subnets
+
+    private_subnet_cidrs = var.private_subnet_cidrs
+
+    # database subnets
+    database_subnet_cidrs = var.database_subnet_cidrs
+}
+
+# data "aws_availability_zones" "available" {
+#   state = "available"
+# }
+
+# o/p checking kosam ila run chesa
+
---
+++ output.tf
@@ -0,0 +1,9 @@
+output "vpc_sri_id" {
+    value = module.vpc_test.vpc_sri_id
+}
+
+# output "awsaz" {
+#   value = data.aws_availability_zones.available
+# }
+
+# mian.tf lo define chesi ikkda o/p check chesa ila evi ayina chudachu
